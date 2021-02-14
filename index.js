f0139dd519d936941ebe39f6497f49ca1310cfab
wmakaben <wmakaben@gmail.com>
2021-02-13 20:55:34 -0500

    Reorganize template

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ import { html, render } from "lit-html";
 import { listTemplate } from "./list.js";
 import { inputTemplate } from "./input.js";
 
+export const myTemplate = () => {
+  return html`
+    <div id="formContainer"></div>
+    <div id="listContainer"></div>
+  `;
+};
+render(myTemplate(), document.body);
+
 const listItems = [
   { text: "Read lit-html docs", isDone: true },
   { text: "Create example thing" }
@@ -13,5 +21,5 @@ const onItemAdded = text => {
   render(listTemplate(listItems), document.getElementById("listContainer"));
 };
 
-render(inputTemplate(onItemAdded), document.body);
+render(inputTemplate(onItemAdded), document.getElementById("formContainer"));
 render(listTemplate(listItems), document.getElementById("listContainer"));
